fdb6f3ebff30b88a87bc8b026775ffd52c892433
Alex Figueiredo <alexxfsilva@gmail.com>
2017-03-19 21:56:12 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const request  = require('request');
+const Forecast = require('forecast');
+
+request.get('http://ipinfo.io/', (err, response, body) => {
+
+    let result   = JSON.parse(body);
+    let location = result.loc.split(',');
+    let forecast = new Forecast({
+        service: 'darksky',
+        key: 'd3782b73349352d1b9e5a4471f394a94',
+        units: 'celcius',
+        cache: true,
+        lang: 'pt'
+    });
+
+    forecast.get(location, (error, weather) => {
+        console.log(weather.daily.summary);
+    });
+});
